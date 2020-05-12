1aba0b68eee44e761356cb3af10b61ac39c7afef
FTima <fateme.ahmadif@gmail.com>
2020-05-12 21:51:27 +0430

    commit

--- dnn_Blocks_Test.py
+++ dnn_Blocks_Test.py
@@ -1,8 +1,7 @@
-from dnn_blocks import *
-from testCases_v4 import *
+import dnn_blocks as model
 
 def test_initialize_parameters():
-    parameters = initialize_parameters(4, 3, 1)
+    parameters = model.initialize_parameters(4, 3, 1)
     assert (parameters['W1'].shape == (3, 4))
     assert (parameters['b1'].shape == (3, 1))
     assert (parameters['W2'].shape == (1, 3))
